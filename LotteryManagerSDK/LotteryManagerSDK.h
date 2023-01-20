9522f5d82075757684173e77601c982fc99f2153
rodrigofranzoi <rodrigo.scroferneker@gmail.com>
2023-01-20 19:15:12 +0000

    Initial Commit

---
+++ LotteryManagerSDK/LotteryManagerSDK.h
@@ -0,0 +1,18 @@
+//
+//  LotteryManagerSDK.h
+//  LotteryManagerSDK
+//
+//  Created by Rodrigo Scroferneker on 20/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LotteryManagerSDK.
+FOUNDATION_EXPORT double LotteryManagerSDKVersionNumber;
+
+//! Project version string for LotteryManagerSDK.
+FOUNDATION_EXPORT const unsigned char LotteryManagerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LotteryManagerSDK/PublicHeader.h>
+
+
