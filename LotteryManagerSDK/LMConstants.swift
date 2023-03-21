e68d8e48f73fdd2163787f67297479c58c61bb6b
Rodrigo Scroferneker <rodrigo.scroferneker@gmail.com>
2023-03-21 13:54:58 +0000

    Add metadata

---
+++ LotteryManagerSDK/LMConstants.swift
@@ -0,0 +1,13 @@
+//
+//  LMConstants.swift
+//  LotteryManagerSDK
+//
+//  Created by Rodrigo Scroferneker on 21/03/2023.
+//
+
+import Foundation
+
+public enum LMConstants {
+  public static let metadataVersion = "1"
+}
+
